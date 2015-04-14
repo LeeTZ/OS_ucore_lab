ad7f0aee835df4da07ef1209ea110574d17ac2b8
LeeTZ <litiezheng513@gmail.com>
2015-04-14 16:38:32 +0800

    finish lab3

--- labcodes/lab3/kern/trap/trap.c
+++ labcodes/lab3/kern/trap/trap.c
@@ -36,7 +36,7 @@ static struct pseudodesc idt_pd = {
 /* idt_init - initialize IDT to each of the entry points in kern/trap/vectors.S */
 void
 idt_init(void) {
-     /* LAB1 YOUR CODE : STEP 2 */
+     /* LAB1 2011011268 : STEP 2 */
      /* (1) Where are the entry addrs of each Interrupt Service Routine (ISR)?
       *     All ISR's entry addrs are stored in __vectors. where is uintptr_t __vectors[] ?
       *     __vectors[] is in kern/trap/vector.S which is produced by tools/vector.c
@@ -48,6 +48,15 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
+    extern uintptr_t __vectors[];
+    int i;
+    for (i = 0; i < 256; ++i)
+        if (i != T_SYSCALL) {
+            SETGATE(idt[i], 0, KERNEL_CS, __vectors[i], DPL_KERNEL);
+        }else {
+            SETGATE(idt[i], 1, KERNEL_CS, __vectors[i], DPL_USER);
+        }
+    lidt(&idt_pd);  
 }
 
 static const char *
@@ -186,6 +195,12 @@ trap_dispatch(struct trapframe *tf) {
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
+         {
+            extern volatile size_t ticks;
+            ++ticks;
+            if (ticks % TICK_NUM == 0) 
+                print_ticks();
+        }
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
